1d816cac2ed4d67024549ee93121672ba5201db0
Mayank <myank07official@gmail.com>
2024-04-18 23:53:32 +0530

    initial commit

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -6,11 +6,11 @@ const Signup = () => {
         <div className="bg-[url('../public/SignUp-bg.png')] min-h-screen w-full bg-cover bg-center flex flex-col-reverse justify-center md:flex lg:flex-row lg:justify-between">
             
             <div className="md:m-auto md:p-10  flex flex-col justify-center  gap-y-3 p-12" >
-                <p className="sm:text-4xl lg:text-6xl md:text-5xl text-center lg:text-left font-bold text-white">Lorem <span className="sm:text-4xl lg:text-6xl md:text-5xl font-bold text-[#74EEDF]">Ipsum</span></p>
+                <p className="text-4xl lg:text-6xl text-center lg:text-left font-bold text-white">Lorem <span className="sm:text-4xl lg:text-6xl md:text-5xl font-bold text-[#74EEDF]">Ipsum</span></p>
                 <p className="text-md text-center lg:text-left max-w-xl text-white px-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed atque, soluta placeat eum suscipit beatae? Atque dolorum </p>
             </div>
 
-            <div className="bg-black backdrop-blur-2xl p-40 max-h-screen sm:w-full lg:w-1/2">
+            <div className="bg-black backdrop-blur-2xl lg:p-40  max-h-screen sm:w-full lg:w-1/2">
                 <div className="flex flex-col justify-center items-center  gap-y-16">
                     <form className="flex flex-col gap-y-12 " action="" method="post">
                         <input placeholder="username" className="align-middle text-center text-black py-3.5 rounded-md md:w-96 w-64"  type="text" name="username" id="" />
