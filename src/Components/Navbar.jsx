cccf56c78dd655a067045055cbb3f58ec5806ba7
Mayank <myank07official@gmail.com>
2024-04-22 13:05:37 +0530

    updated navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <nav className="flex justify-between bg-black w-full p-7">
-            <p className="text-3xl text-white md:text-4xl font-bold ml-9">Lorem</p>
+            <p className="text-xl text-white md:text-4xl font-bold md:ml-9">Lorem</p>
 
             <ul className="hidden md:flex mt-2">
                 <a className="px-10 text-white text-lg cursor-pointer">Home</a>
