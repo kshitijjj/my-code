c2d0e46e96c894f6f4c751e4241556fe20ddbad9
Mayank <myank07official@gmail.com>
2024-04-22 13:14:52 +0530

    updated Login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,14 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className="bg-[url('../public/SignUp-bg.png')] min-h-screen w-full bg-cover bg-center flex flex-col-reverse justify-center md:flex lg:flex-row-reverse lg:justify-between">
+        <div className="bg-[url('../public/SignUp-bg.png')] min-h-screen w-full bg-cover bg-center flex flex-col justify-center md:flex lg:flex-row lg:justify-between">
             
-            <div className="md:m-auto md:p-10  flex flex-col justify-center  gap-y-3 p-12" >
-                <p className="sm:text-3xl lg:text-6xl md:text-5xl text-center lg:text-left font-bold text-white">Lorem <span className="sm:text-3xl lg:text-6xl md:text-5xl font-bold text-[#74EEDF]">Ipsum</span></p>
-                <p className="text-md text-center lg:text-left max-w-xl text-white px-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed atque, soluta placeat eum suscipit beatae? Atque dolorum </p>
-            </div>
-
-            <div className="bg-black backdrop-blur-2xl p-40 max-h-screen sm:w-full lg:w-1/2">
+            <div className=" bg-black backdrop-blur-2xl md:p-40 max-h-screen sm:w-full lg:w-1/2">
                 <div className="flex flex-col justify-center items-center  gap-y-16">
                     <form className="flex flex-col gap-y-12 " action="" method="post">
                         <input placeholder="username" className="align-middle text-center text-black py-3.5 rounded-md md:w-96 w-64"  type="text" name="username" id="" />
@@ -25,6 +20,14 @@ const Login = () => {
                     <Link className="text-[#74EEDF] whitespace-nowrap text-lg" to="/signup">Sign up</Link>
                 </div>
             </div>
+
+
+            <div className="md:m-auto md:p-10  flex flex-col justify-center  gap-y-3 p-12" >
+                <p className="text-4xl lg:text-6xl md:text-5xl text-center lg:text-left font-bold text-white">Lorem <span className="sm:text-3xl lg:text-6xl md:text-5xl font-bold text-[#74EEDF]">Ipsum</span></p>
+                <p className="text-md text-center lg:text-left max-w-xl text-white px-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed atque, soluta placeat eum suscipit beatae? Atque dolorum </p>
+            </div>
+
+            
         </div>
     );
 }
