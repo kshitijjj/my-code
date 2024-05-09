7ace9c8ba9211542ff268080dd56bf9562528c3c
Mayank <myank07official@gmail.com>
2024-05-09 18:45:25 +0530

    updated login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -30,25 +30,27 @@ const Login = () => {
     return (
         <>
 
-        <div className="flex items-center gap-2 pt-[18px] pl-[24px] md:hidden">
+        <div className="flex items-center gap-2 pt-[18px] md:pl-[3.4rem] md:pt-[1.5rem]">
                 <img src="../Logo.png" alt="" width={"24.32px"} height={"24.32px"}/>
                 <p className="font-poppins font-extrabold text-lg md:text-2xl">Lorem</p>
         </div>
 
-        <div className="flex flex-col ml-[1.2rem] mt-[1rem]" >
-            <div>
-                <div className="flex flex-col gap-2 mt-[3.5rem]">
-                    <p className="text-4xl font-extrabold leading-7">Welcome to </p>
-                    <p className="text-[#2F88FF] text-4xl font-extrabold">Lorem Ipsum</p>
-                    <p className="max-w-xs text-[#000000] text-opacity-45">Lorem ipsum is a dummy text which i don’t know what to right here so i am writing a random text here bjdcdchdic</p>
+        <div className="" >
+            <div className="flex flex-col md:flex-row ml-[1.2rem] mt-[1rem] md:ml-[3rem] md:justify-between ">
+                <div className="flex flex-col gap-2 md:gap-6 mt-[3.5rem] md:justify-center md:h-[30rem]" >
+                    <div>
+                    <p className="text-4xl md:text-[4rem] md:leading-[5rem] font-extrabold leading-7">Welcome to </p>
+                    <p className="text-[#2F88FF] text-4xl md:text-[4rem] font-extrabold">Lorem Ipsum</p>
+                    </div>
+                    <p className="max-w-xs text-[#000000] text-opacity-45 md:max-w-2xl md:text-[1.3rem]">Lorem ipsum is a dummy text which i don’t know what to right here so i am writing a random text here bjdcdchdic Lorem ipsum is a dummy text which i don’t know what to right here so i am writing a random text here bjdcdchdic  </p>
                 </div>
 
-                <div className="flex flex-col gap-8 pr-16 mt-[3rem]">
-                    <input className="border py-[1.2rem] pl-2 rounded-lg" type="text" placeholder='Enter your email' />
-                    <input className="border py-[1.2rem] pl-2 rounded-lg" type="text" name="" id="" placeholder="Enter your password" />
+                <div className="flex flex-col gap-8 pr-16 mt-[3rem] md:justify-center md:h-[35rem] md:items-center md:pr-0 md:mr-[12rem] md:gap-12">
+                    <input className="border py-[1.2rem] pl-2 rounded-lg w-[25.75rem]" type="text" placeholder='Enter your email' />
+                    <input className="border py-[1.2rem] pl-2 rounded-lg w-[25.75rem]" type="text" name="" id="" placeholder="Enter your password" />
 
                     <div className="mt-[2rem]">
-                        <a className="bg-[#2F88FF] text-white  font-bold px-[3rem] py-[1rem] rounded-md" href="">Login </a>
+                        <a className="bg-[#2F88FF] text-white  font-bold px-[3rem] py-[1rem] md:px-[6.5rem] md:text-md rounded-md" href="">Login </a>
                     </div>
 
                     <div className="mt-[2rem]">
@@ -56,7 +58,6 @@ const Login = () => {
                     </div>
                 </div>
             </div>
-            <div></div>
         </div>
         
         </>
