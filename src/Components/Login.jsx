96cfc987fdd59844de0b375b7855fe05805e93e0
Mayank <myank07official@gmail.com>
2024-05-09 12:41:51 +0530

    updated login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios";
 
 const Login = () => {
-    const navigate=useNavigate();
+   /*  const navigate=useNavigate();
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
     const [btntext,setBtntext]=useState("Login");
@@ -25,35 +25,40 @@ const Login = () => {
             }
         }, 2000);
         
-    }
+    } */
 
     return (
-        <div className="min-h-screen w-full bg-cover bg-center flex flex-col justify-center md:flex lg:flex-row lg:justify-between">
-            
-            <div className=" bg-black bg-opacity-25 md:p-40 max-h-screen sm:w-full lg:w-1/2">
-                <div className="flex flex-col justify-center items-center  gap-y-16">
-                    <form className="flex flex-col gap-y-12 " action="" method="post" onSubmit={handleSubmit}>
-                        <input onChange={(e)=>{setEmail(e.target.value)}} placeholder="email" className="align-middle text-center text-black py-3.5 rounded-md md:w-96 w-64"  type="email" name="username" id="" />
-                        <input onChange={(e)=>{setPassword(e.target.value)}} placeholder="password" className="align-middle text-center text-black py-3.5 rounded-md md:w-96 w-64" type="password" name="username" id="" />
-                        <button className="text-lg md:text-lg rounded-lg border-[#74EEDF] text-white border-[1.5px] hover:bg-[#74EEDF] hover:text-black px-12 py-2.5 whitespace-nowrap" >{btntext}</button>
-                    </form>
+        <>
 
-                </div>
-
-                <div className="flex justify-center mt-8 py-8">
-                    <p className="text-white px-3 text-lg whitespace-nowrap">Already have an account ?</p>
-                    <Link className="text-[#74EEDF] whitespace-nowrap text-lg" to="/signup">Sign up</Link>
-                </div>
-            </div>
-
-
-            <div className="md:m-auto md:p-10  flex flex-col justify-center  gap-y-3 p-12" >
-                <p className="text-4xl lg:text-6xl md:text-5xl text-center lg:text-left font-bold text-white">Lorem <span className="sm:text-3xl lg:text-6xl md:text-5xl font-bold text-[#74EEDF]">Ipsum</span></p>
-                <p className="text-md text-center lg:text-left max-w-xl text-white px-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed atque, soluta placeat eum suscipit beatae? Atque dolorum </p>
-            </div>
-
-            
+        <div className="flex items-center gap-2 pt-[18px] pl-[24px] md:hidden">
+                <img src="../Logo.png" alt="" width={"24.32px"} height={"24.32px"}/>
+                <p className="font-poppins font-extrabold text-lg md:text-2xl">Lorem</p>
         </div>
+
+        <div className="flex flex-col ml-[1.2rem] mt-[3rem]" >
+            <div>
+                <div className="flex flex-col gap-2 mt-[3.5rem]">
+                    <p className="text-4xl font-extrabold max-w-xs leading-7">Welcome to <span className="text-[#2F88FF] text-4xl font-extrabold">Lorem Ipsum</span></p>
+                    <p className="max-w-xs text-[#000000] text-opacity-45">Lorem ipsum is a dummy text which i don’t know what to right here so i am writing a random text here bjdcdchdic</p>
+                </div>
+
+                <div className="flex flex-col gap-8 pr-24 mt-[3rem]">
+                    <input className="border py-[0.8rem] pl-2 rounded-lg" type="text" placeholder='Enter your email' />
+                    <input className="border py-[0.8rem] pl-2 rounded-lg" type="text" name="" id="" placeholder="Enter your password" />
+
+                    <div className="mt-[2rem]">
+                        <a className="bg-[#2F88FF] text-white font-bold px-14 py-2 rounded-md" href="">Login </a>
+                    </div>
+
+                    <div className="mt-[2rem]">
+                        <p>Don't have an account?&nbsp;<span className="text-[#2F88FF] font-bold">Sign Up</span></p>
+                    </div>
+                </div>
+            </div>
+            <div></div>
+        </div>
+        
+        </>
     );
 }
 
