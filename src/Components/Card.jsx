33f63970d1b6ea28476c1739513977f7ecad1be4
Mayank <myank07official@gmail.com>
2024-05-10 00:22:04 +0530

    updated home component image

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,7 +4,7 @@ function Card (props){
     return (
        <>
        <div className="flex flex-col rounded-lg mt-[4rem] md:max-w-md">
-            <img src="../1.png" alt="" srcset="" />
+            <img className="h-[11.25rem] md:h-[13.25rem]"   src="../1.png" alt="" srcset="" height={"11.25rem"}/>
             <div className="pt-[0.8rem] px-[1rem]">
                 <p className="text-[#2F88FF] font-bold text-2xl leading-10">Slinkify</p>
                 <p className="text-md ">Lorem ipsum is a dummy text which i don’t know what to right here so i am writing a random text here bjdcdchdic Lorem ipsum</p>
